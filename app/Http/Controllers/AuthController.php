9c04cb3a671e621b2b03cd477e6623fa41addcfe
IndunilDhanaranga <indunil.xypherlabs@gmail.com>
2024-03-18 19:54:17 +0530

    new login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use Auth;
+
+use App\Models\User;
+
+class AuthController extends Controller {
+    /*
+    ----------------------------------------------------------------------------------------------------------
+    PUBLIC FUNCTION DOLOGIN
+    ----------------------------------------------------------------------------------------------------------
+    */
+
+    public function dologin(Request $request) {
+        try {
+
+            $validator = Validator::make( $request->all(),[
+                'email' => 'required|email',
+                'password' => 'required',
+            ] );
+
+            if($validator->fails()){
+                return redirect()->route('login')->withErrors([$validator->messages()->all()]);
+            }
+
+            $userDetails = array(
+                'email' => $request->email,
+                'password' => $request->password,
+            );
+
+            if(Auth::attempt($userDetails)){
+                if(User::where('id',Auth::id())->exists()){
+                    if(User::where('is_active',0)->exists()){
+                        Auth::logout();
+                        return redirect()->route('login')->withErrors(["Your no longer available.Please contact system adminstrator."]);
+                    }
+                    $user = User::where( 'id', Auth::id() )->first();
+                    return redirect()->route('dashboard');
+                }
+                Auth::logout();
+                return redirect()->route( 'login' )->withErrors( [ 'Your Not Available.' ] );
+            }
+            Auth::logout();
+            return redirect()->route( 'login' )->withErrors( [ 'email or password does not exist.' ] );
+
+        } catch ( \Throwable $th ) {
+            return $th->getMessage();
+        }
+    }
+
+    /*
+    ----------------------------------------------------------------------------------------------------------
+    PUBLIC FUNCTION DOLOGOUT
+    ----------------------------------------------------------------------------------------------------------
+    */
+
+    public function dologout() {
+        try {
+
+            Auth::logout();
+            return redirect()->route( 'login' );
+
+        } catch ( \Throwable $th ) {
+            return $th->getMessage();
+        }
+    }
+}
--- resources/views/back-end/login.blade.php
+++ resources/views/back-end/login.blade.php
@@ -27,9 +27,10 @@
             <div class="card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
 
-                <form action="/assets/back-end/default/index3.html" method="post">
+                <form action="/signin" method="post">
+                    @csrf
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
+                        <input type="email" class="form-control" name="email" placeholder="Email">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
@@ -37,22 +38,23 @@
                         </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password">
+                        <input type="password" class="form-control" name="password" placeholder="Password">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="icheck-primary">
-                                <input type="checkbox" id="remember">
-                                <label for="remember">
-                                    Remember Me
-                                </label>
+                    @if ($errors->any())
+                        @foreach ($errors->all() as $error)
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <div class="font-weight-bold text-danger ml-2">{{ $error }}</div>
+                                </div>
                             </div>
-                        </div>
+                        @endforeach
+                    @endif
+                    <div class="row">
                         <!-- /.col -->
                         <div class="col-4">
                             <button type="submit" class="btn btn-primary btn-block">Sign In</button>
@@ -60,23 +62,6 @@
                         <!-- /.col -->
                     </div>
                 </form>
-
-                <div class="social-auth-links text-center mt-2 mb-3">
-                    <a href="#" class="btn btn-block btn-primary">
-                        <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-                    </a>
-                    <a href="#" class="btn btn-block btn-danger">
-                        <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-                    </a>
-                </div>
-                <!-- /.social-auth-links -->
-
-                <p class="mb-1">
-                    <a href="forgot-password.html">I forgot my password</a>
-                </p>
-                <p class="mb-0">
-                    <a href="register.html" class="text-center">Register a new membership</a>
-                </p>
             </div>
             <!-- /.card-body -->
         </div>
