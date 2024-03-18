ea41ba1946fbf3b662d6d0ce8513eb8d70b4a967
IndunilDhanaranga <indunil.xypherlabs@gmail.com>
2024-03-18 18:40:51 +0530

    new

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -7,6 +7,16 @@ use Illuminate\Support\Facades\View;
 
 class ViewController extends DataController
 {
+
+    /*---------------------------------------------------------------------------------------------------------------------------------------------------------- */
+                                                                        /*FRONT-END */
+    /*---------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+    /*
+    ----------------------------------------------------------------------------------------------------------------------------------
+    START FRONT-END VIEW
+    ----------------------------------------------------------------------------------------------------------------------------------
+    */
     public function frontend( ) {
 
         $css = array(
@@ -28,4 +38,82 @@ class ViewController extends DataController
         $data['script'] = $script;
         return view::make( 'front-end.home', $data );
     }
+    /*
+    ----------------------------------------------------------------------------------------------------------------------------------
+    END FRONT-END VIEW
+    ----------------------------------------------------------------------------------------------------------------------------------
+    */
+
+
+    /*---------------------------------------------------------------------------------------------------------------------------------------------------------- */
+                                                                        /*BACK-END */
+    /*---------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+
+    /*
+    ----------------------------------------------------------------------------------------------------------
+    PUBLIC FUNCTION VIEW DEFAULT
+    ----------------------------------------------------------------------------------------------------------
+    */
+
+    public function default( $data ) {
+
+        $css = array(
+            config( 'site-specific.google-font-css' ),
+            config( 'site-specific.all-min-css' ),
+            config( 'site-specific.icon-min-css' ),
+            config( 'site-specific.tempusdominus-bootstrap-min-css' ),
+            config( 'site-specific.icheck-bootstrap-min-css' ),
+            config( 'site-specific.jqvmap-min-css' ),
+            config( 'site-specific.adminlte-min-css' ),
+            config( 'site-specific.OverlayScrollbars-min-css' ),
+            config( 'site-specific.daterangepicker-min-css' ),
+            config( 'site-specific.summernote-min-css' ),
+        );
+
+        $script = array(
+            config( 'site-specific.jquery-min-js' ),
+            config( 'site-specific.jquery-ui-min-js' ),
+            config( 'site-specific.bootstrap-bundle-min-js' ),
+            config( 'site-specific.Chart-min-js' ),
+            config( 'site-specific.sparkline-min-js' ),
+            config( 'site-specific.jquery-vmap-min-js' ),
+            config( 'site-specific.jquery-vmap-usa-min-js' ),
+            config( 'site-specific.jquery-knob-min-js' ),
+            config( 'site-specific.moment-min-js' ),
+            config( 'site-specific.daterangepicker-min-js' ),
+            config( 'site-specific.tempusdominus-bootstrap-min-js' ),
+            config( 'site-specific.summernote-min-js' ),
+            config( 'site-specific.jquery-overlayScrollbars-min-js' ),
+            config( 'site-specific.adminlte-min-js' ),
+            config( 'site-specific.dashboard-min-js' ),
+        );
+
+        if ( isset( $data[ 'css' ] ) ) {
+            $data[ 'css' ]    = array_merge( $css, $data[ 'css' ] );
+        } else {
+            $data[ 'css' ]    = $css;
+        }
+        if ( isset( $data[ 'script' ] ) ) {
+            $data[ 'script' ] = array_merge( $script, $data[ 'script' ] );
+        } else {
+            $data[ 'script' ] = $script;
+        }
+        return view::make( 'back-end.home', $data );
+    }
+
+    /*
+    ----------------------------------------------------------------------------------------------------------
+    PUBLIC FUNCTION VIEW DASHBORD
+    ----------------------------------------------------------------------------------------------------------
+    */
+
+    public function dashboard(){
+        $data = [
+            'view' =>'hello',
+        ];
+
+        return $this->default($data);
+    }
+
 }
--- config/site-specific.php
+++ config/site-specific.php
@@ -36,5 +36,75 @@ return [
         'google-map'                    => 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCtme10pzgKSPeJVJrG1O3tjR6lk98o4w8&callback=initMap',
 
         'johnedoe-init-js'              => '/assets/front-end/default/js/johndoe.js',
-        
+
+
+    /*
+    ----------------------------------------------------------------------------------------------------------------------------------
+    Back End
+    ----------------------------------------------------------------------------------------------------------------------------------
+    */
+
+        /*
+        ----------------------------------------------------------------------------------------------------------------------------------
+        Default CSS
+        ----------------------------------------------------------------------------------------------------------------------------------
+        */
+
+
+        'google-font-css'       =>'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback',
+
+        'all-min-css'       =>'/assets/back-end/default/plugins/fontawesome-free/css/all.min.css',
+
+        'icon-min-css'       =>'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
+
+        'tempusdominus-bootstrap-min-css'       =>'/assets/back-end/default/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css',
+
+        'icheck-bootstrap-min-css'       =>'/assets/back-end/default/plugins/icheck-bootstrap/icheck-bootstrap.min.css',
+
+        'jqvmap-min-css'       =>'/assets/back-end/default/plugins/jqvmap/jqvmap.min.css',
+
+        'adminlte-min-css'       =>'/assets/back-end/default/dist/css/adminlte.min.css',
+
+        'OverlayScrollbars-min-css'       =>'/assets/back-end/default/plugins/overlayScrollbars/css/OverlayScrollbars.min.css',
+
+        'daterangepicker-min-css'       =>'/assets/back-end/default/plugins/daterangepicker/daterangepicker.css',
+
+        'summernote-min-css'       =>'/assets/back-end/default/plugins/summernote/summernote-bs4.min.css',
+
+        /*
+        ----------------------------------------------------------------------------------------------------------------------------------
+        Default JAVA SCRIPT
+        ----------------------------------------------------------------------------------------------------------------------------------
+        */
+
+        'jquery-min-js'       =>'/assets/back-end/default/plugins/jquery/jquery.min.js',
+
+        'jquery-ui-min-js'       =>'/assets/back-end/default/plugins/jquery-ui/jquery-ui.min.js',
+
+        'bootstrap-bundle-min-js'       =>'/assets/back-end/default/plugins/bootstrap/js/bootstrap.bundle.min.js',
+
+        'Chart-min-js'       =>'/assets/back-end/default/plugins/chart.js/Chart.min.js',
+
+        'sparkline-min-js'       =>'/assets/back-end/default/plugins/sparklines/sparkline.js',
+
+        'jquery-vmap-min-js'       =>'/assets/back-end/default/plugins/jqvmap/jquery.vmap.min.js',
+
+        'jquery-vmap-usa-min-js'       =>'/assets/back-end/default/plugins/jqvmap/maps/jquery.vmap.usa.js',
+
+        'jquery-knob-min-js'       =>'/assets/back-end/default/plugins/jquery-knob/jquery.knob.min.js',
+
+        'moment-min-js'       =>'/assets/back-end/default/plugins/moment/moment.min.js',
+
+        'daterangepicker-min-js'       =>'/assets/back-end/default/plugins/daterangepicker/daterangepicker.js',
+
+        'tempusdominus-bootstrap-min-js'       =>'/assets/back-end/default/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
+
+        'summernote-min-js'       =>'/assets/back-end/default/plugins/summernote/summernote-bs4.min.js',
+
+        'jquery-overlayScrollbars-min-js'       =>'/assets/back-end/default/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js',
+
+        'adminlte-min-js'       =>'/assets/back-end/default/dist/js/adminlte.js',
+
+        'dashboard-min-js'       =>'/assets/back-end/default/dist/js/pages/dashboard.js',
+
 ];
