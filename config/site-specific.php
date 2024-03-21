d25f43dd64b20bd219dbdb3ed78a45b7868ce11f
IndunilDhanaranga <indunil.xypherlabs@gmail.com>
2024-03-21 19:32:53 +0530

    new

--- config/site-specific.php
+++ config/site-specific.php
@@ -8,37 +8,35 @@ return [
     ----------------------------------------------------------------------------------------------------------------------------------
     */
 
-        /*
-        ----------------------------------------------------------------------------------------------------------------------------------
-        Default CSS
-        ----------------------------------------------------------------------------------------------------------------------------------
-        */
+    /*
+    ----------------------------------------------------------------------------------------------------------------------------------
+    Default CSS
+    ----------------------------------------------------------------------------------------------------------------------------------
+    */
 
-        'themify-icons-css'             => '/assets/front-end/default/vendors/themify-icons/css/themify-icons.css',
+    'themify-icons-css'                 => '/assets/front-end/default/vendors/themify-icons/css/themify-icons.css',
 
-        'johndoe-css'                   => '/assets/front-end/default/css/johndoe.css',
+    'johndoe-css'                       => '/assets/front-end/default/css/johndoe.css',
 
-        'fontawesome-css'                   => 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
+    'fontawesome-css'                   => 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
 
+    /*
+    ----------------------------------------------------------------------------------------------------------------------------------
+    Default JAVA SCRIPT
+    ----------------------------------------------------------------------------------------------------------------------------------
+    */
 
-        /*
-        ----------------------------------------------------------------------------------------------------------------------------------
-        Default JAVA SCRIPT
-        ----------------------------------------------------------------------------------------------------------------------------------
-        */
+    'jquery-init-js'                    => '/assets/front-end/default/vendors/jquery/jquery-3.4.1.js',
 
-        'jquery-init-js'                => '/assets/front-end/default/vendors/jquery/jquery-3.4.1.js',
+    'bootstrap-bundle-init-js'          => '/assets/front-end/default/vendors/bootstrap/bootstrap.bundle.js',
 
-        'bootstrap-bundle-init-js'      => '/assets/front-end/default/vendors/bootstrap/bootstrap.bundle.js',
+    'bootstrap-affix-init-js'           => '/assets/front-end/default/vendors/bootstrap/bootstrap.affix.js',
 
-        'bootstrap-affix-init-js'       => '/assets/front-end/default/vendors/bootstrap/bootstrap.affix.js',
+    'isotope-package-init-js'           => '/assets/front-end/default/vendors/isotope/isotope.pkgd.js',
 
-        'isotope-package-init-js'       => '/assets/front-end/default/vendors/isotope/isotope.pkgd.js',
+    'google-map'                        => 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCtme10pzgKSPeJVJrG1O3tjR6lk98o4w8&callback=initMap',
 
-        'google-map'                    => 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCtme10pzgKSPeJVJrG1O3tjR6lk98o4w8&callback=initMap',
-
-        'johnedoe-init-js'              => '/assets/front-end/default/js/johndoe.js',
-
+    'johnedoe-init-js'                  => '/assets/front-end/default/js/johndoe.js',
 
     /*
     ----------------------------------------------------------------------------------------------------------------------------------
@@ -46,106 +44,100 @@ return [
     ----------------------------------------------------------------------------------------------------------------------------------
     */
 
-        /*
-        ----------------------------------------------------------------------------------------------------------------------------------
-        Default CSS
-        ----------------------------------------------------------------------------------------------------------------------------------
-        */
+    /*
+    ----------------------------------------------------------------------------------------------------------------------------------
+    Default CSS
+    ----------------------------------------------------------------------------------------------------------------------------------
+    */
 
+    'google-font-css'                   =>'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback',
 
-        'google-font-css'       =>'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback',
+    'all-min-css'                       =>'/assets/back-end/default/plugins/fontawesome-free/css/all.min.css',
 
-        'all-min-css'       =>'/assets/back-end/default/plugins/fontawesome-free/css/all.min.css',
+    'icon-min-css'                      =>'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
 
-        'icon-min-css'       =>'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
+    'tempusdominus-bootstrap-min-css'   =>'/assets/back-end/default/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css',
 
-        'tempusdominus-bootstrap-min-css'       =>'/assets/back-end/default/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css',
+    'icheck-bootstrap-min-css'          =>'/assets/back-end/default/plugins/icheck-bootstrap/icheck-bootstrap.min.css',
 
-        'icheck-bootstrap-min-css'       =>'/assets/back-end/default/plugins/icheck-bootstrap/icheck-bootstrap.min.css',
+    'jqvmap-min-css'                    =>'/assets/back-end/default/plugins/jqvmap/jqvmap.min.css',
 
-        'jqvmap-min-css'       =>'/assets/back-end/default/plugins/jqvmap/jqvmap.min.css',
+    'adminlte-min-css'                  =>'/assets/back-end/default/dist/css/adminlte.min.css',
 
-        'adminlte-min-css'       =>'/assets/back-end/default/dist/css/adminlte.min.css',
+    'OverlayScrollbars-min-css'         =>'/assets/back-end/default/plugins/overlayScrollbars/css/OverlayScrollbars.min.css',
 
-        'OverlayScrollbars-min-css'       =>'/assets/back-end/default/plugins/overlayScrollbars/css/OverlayScrollbars.min.css',
+    'daterangepicker-min-css'           =>'/assets/back-end/default/plugins/daterangepicker/daterangepicker.css',
 
-        'daterangepicker-min-css'       =>'/assets/back-end/default/plugins/daterangepicker/daterangepicker.css',
+    'summernote-min-css'                =>'/assets/back-end/default/plugins/summernote/summernote-bs4.min.css',
 
-        'summernote-min-css'       =>'/assets/back-end/default/plugins/summernote/summernote-bs4.min.css',
+    'sweetalert-css'                    => '/assets/back-end/default/plugins/sweetalert2/sweetalert2.min.css',
 
-        'sweetalert-css'                        => '/assets/back-end/default/plugins/sweetalert2/sweetalert2.min.css',
+    'toastr-css'                        => '/assets/back-end/default/plugins/toastr/toastr.min.css',
 
-        'toastr-css'                            => '/assets/back-end/default/plugins/toastr/toastr.min.css',
+    'select2-css'                       => '/assets/back-end/default/plugins/select2/css/select2.min.css',
 
-        'select2-css'                           => '/assets/back-end/default/plugins/select2/css/select2.min.css',
+    'select2-bootstrap4-css'            => '/assets/back-end/default/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css',
 
-        'select2-bootstrap4-css'                => '/assets/back-end/default/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css',
+    /*
+    ----------------------------------------------------------------------------------------------------------------------------------
+    Default JAVA SCRIPT
+    ----------------------------------------------------------------------------------------------------------------------------------
+    */
 
+    'jquery-min-js'                     =>'/assets/back-end/default/plugins/jquery/jquery.min.js',
 
-        /*
-        ----------------------------------------------------------------------------------------------------------------------------------
-        Default JAVA SCRIPT
-        ----------------------------------------------------------------------------------------------------------------------------------
-        */
+    'jquery-ui-min-js'                  =>'/assets/back-end/default/plugins/jquery-ui/jquery-ui.min.js',
 
-        'jquery-min-js'       =>'/assets/back-end/default/plugins/jquery/jquery.min.js',
+    'bootstrap-bundle-min-js'           =>'/assets/back-end/default/plugins/bootstrap/js/bootstrap.bundle.min.js',
 
-        'jquery-ui-min-js'       =>'/assets/back-end/default/plugins/jquery-ui/jquery-ui.min.js',
+    'Chart-min-js'                      =>'/assets/back-end/default/plugins/chart.js/Chart.min.js',
 
-        'bootstrap-bundle-min-js'       =>'/assets/back-end/default/plugins/bootstrap/js/bootstrap.bundle.min.js',
+    'sparkline-min-js'                  =>'/assets/back-end/default/plugins/sparklines/sparkline.js',
 
-        'Chart-min-js'       =>'/assets/back-end/default/plugins/chart.js/Chart.min.js',
+    'jquery-vmap-min-js'                =>'/assets/back-end/default/plugins/jqvmap/jquery.vmap.min.js',
 
-        'sparkline-min-js'       =>'/assets/back-end/default/plugins/sparklines/sparkline.js',
+    'jquery-vmap-usa-min-js'            =>'/assets/back-end/default/plugins/jqvmap/maps/jquery.vmap.usa.js',
 
-        'jquery-vmap-min-js'       =>'/assets/back-end/default/plugins/jqvmap/jquery.vmap.min.js',
+    'jquery-knob-min-js'                =>'/assets/back-end/default/plugins/jquery-knob/jquery.knob.min.js',
 
-        'jquery-vmap-usa-min-js'       =>'/assets/back-end/default/plugins/jqvmap/maps/jquery.vmap.usa.js',
+    'moment-min-js'                     =>'/assets/back-end/default/plugins/moment/moment.min.js',
 
-        'jquery-knob-min-js'       =>'/assets/back-end/default/plugins/jquery-knob/jquery.knob.min.js',
+    'daterangepicker-min-js'            =>'/assets/back-end/default/plugins/daterangepicker/daterangepicker.js',
 
-        'moment-min-js'       =>'/assets/back-end/default/plugins/moment/moment.min.js',
+    'tempusdominus-bootstrap-min-js'    =>'/assets/back-end/default/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
 
-        'daterangepicker-min-js'       =>'/assets/back-end/default/plugins/daterangepicker/daterangepicker.js',
+    'summernote-min-js'                 =>'/assets/back-end/default/plugins/summernote/summernote-bs4.min.js',
 
-        'tempusdominus-bootstrap-min-js'       =>'/assets/back-end/default/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
+    'jquery-overlayScrollbars-min-js'   =>'/assets/back-end/default/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js',
 
-        'summernote-min-js'       =>'/assets/back-end/default/plugins/summernote/summernote-bs4.min.js',
+    'adminlte-min-js'                   =>'/assets/back-end/default/dist/js/adminlte.js',
 
-        'jquery-overlayScrollbars-min-js'       =>'/assets/back-end/default/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js',
+    'dashboard-min-js'                  =>'/assets/back-end/default/dist/js/pages/dashboard.js',
 
-        'adminlte-min-js'       =>'/assets/back-end/default/dist/js/adminlte.js',
+    'sweetalert2-js'                    => '/assets/back-end/default/plugins/sweetalert2/sweetalert2.min.js',
 
-        'dashboard-min-js'       =>'/assets/back-end/default/dist/js/pages/dashboard.js',
+    'toastr-js'                         => '/assets/back-end/default/plugins/toastr/toastr.min.js',
 
-        'sweetalert2-js'                        => '/assets/back-end/default/plugins/sweetalert2/sweetalert2.min.js',
+    'select2-js'                        => '/assets/back-end/default/plugins/select2/js/select2.full.min.js',
 
-        'toastr-js'                             => '/assets/back-end/default/plugins/toastr/toastr.min.js',
+    /*
+    ----------------------------------------------------------------------------------------------------------------------------------
+    CUSTOM JAVA SCRIPT
+    ----------------------------------------------------------------------------------------------------------------------------------
+    */
 
-        'select2-js'                            => '/assets/back-end/default/plugins/select2/js/select2.full.min.js',
+    'basic-details-init-js'             =>'/assets/back-end/custom/js/basic-details-init.js',
 
+    'schools-collages-init-js'          =>'/assets/back-end/custom/js/school-collages-init.js',
 
+    'education-levels-init-js'          =>'/assets/back-end/custom/js/education-levels-init.js',
 
-        /*
-        ----------------------------------------------------------------------------------------------------------------------------------
-        CUSTOM JAVA SCRIPT
-        ----------------------------------------------------------------------------------------------------------------------------------
-        */
+    'education-qualification-init-js'   =>'/assets/back-end/custom/js/education-qualification-init.js',
 
-        'basic-details-init-js'       =>'/assets/back-end/custom/js/basic-details-init.js',
+    'expertise-init-js'                 =>'/assets/back-end/custom/js/expertise-init.js',
 
-        'schools-collages-init-js'       =>'/assets/back-end/custom/js/school-collages-init.js',
+    'additional-details-init-js'        =>'/assets/back-end/custom/js/additional-details-init.js',
 
-        'education-levels-init-js'       =>'/assets/back-end/custom/js/education-levels-init.js',
-
-        'education-qualification-init-js'       =>'/assets/back-end/custom/js/education-qualification-init.js',
-
-        'expertise-init-js'       =>'/assets/back-end/custom/js/expertise-init.js',
-
-        'additional-details-init-js'       =>'/assets/back-end/custom/js/additional-details-init.js',
-
-
-
-        'live-path' => ''
+    'live-path'                         => ''
 
 ];
