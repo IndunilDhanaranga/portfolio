d074cebcb97008eb0262dcafdcdf6c43ee5b7487
IndunilDhanaranga <indunil.xypherlabs@gmail.com>
2024-03-22 16:06:22 +0530

    new

--- public/assets/back-end/custom/js/user-roll-init.js
+++ public/assets/back-end/custom/js/user-roll-init.js
@@ -9,26 +9,26 @@ $("#user_roll")
     .container()
     .appendTo("#user_roll_wrapper .col-md-6:eq(0)");
 
-    $( document ).ready(function() {
-
-        $("#user-role tbody tr").each(function () {
-            var allchecked = true;
-            $(this).find(".form-check-input").each(function () {
-                if(!$(this).is(':checked')){
+$(document).ready(function () {
+    $("#user-role tbody tr").each(function () {
+        var allchecked = true;
+        $(this)
+            .find(".form-check-input")
+            .each(function () {
+                if (!$(this).is(":checked")) {
                     allchecked = false;
                 }
             });
-            if(allchecked == true){
-                $(this).find(".select-all").prop('checked',true)
-            }
-        });
-    });
-
-    function selectAll(ele) {
-        if($(ele).is(':checked')) {
-          $(ele).closest("tr").find(".form-check-input").prop('checked',true);
-        } else {
-          $(ele).closest("tr").find(".form-check-input").prop('checked',false);
+        if (allchecked == true) {
+            $(this).find(".select-all").prop("checked", true);
         }
-    }
+    });
+});
 
+function selectAll(ele) {
+    if ($(ele).is(":checked")) {
+        $(ele).closest("tr").find(".form-check-input").prop("checked", true);
+    } else {
+        $(ele).closest("tr").find(".form-check-input").prop("checked", false);
+    }
+}
