65a2139cc6801edead0bdae67aec39454243ab99
IndunilDhanaranga <indunil.xypherlabs@gmail.com>
2024-03-30 10:22:09 +0530

    new

--- resources/views/back-end/user.blade.php
+++ resources/views/back-end/user.blade.php
@@ -72,10 +72,10 @@
                     </div>
                     <div class="row">
                         <div class="col-12">
-                            <div class="form-group" data-select2-id="2">
+                            <div class="form-group" data-select2-id="22">
                                 <label for="user_roll">User Roll</label>
                                 <select class="form-control select2 " name="user_roll" style="width: 100%;"
-                                    data-select2-id="2" aria-hidden="true">
+                                    data-select2-id="22" aria-hidden="true">
                                     <option value="">No Select</option>
                                     @foreach ($user_roll as $item)
                                         <option value="{{ $item->id }}">{{ $item->title }}</option>
